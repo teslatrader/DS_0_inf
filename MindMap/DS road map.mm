1a4045e077db717550e4edd07b95005410244ae0
teslatrader <2724546@gmail.com>
2023-02-24 02:15:07 +0600

    1st

---
+++ MindMap/DS road map.mm
@@ -0,0 +1,181 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#0033ff" CREATED="1677179262993" ID="ID_19045468" MODIFIED="1677182407835" TEXT="DS road map">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+<node COLOR="#00b439" CREATED="1677179295555" ID="ID_1264945826" MODIFIED="1677182188049" POSITION="right" TEXT="&#x422;&#x435;&#x445;&#x43d;&#x438;&#x447;&#x435;&#x441;&#x43a;&#x438;&#x435; &#x43d;&#x430;&#x432;&#x44b;&#x43a;&#x438;">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#990000" CREATED="1677179586234" ID="ID_1045844302" MODIFIED="1677182188050" TEXT="&#x431;&#x430;&#x437;&#x43e;&#x432;&#x44b;&#x435;">
+<edge COLOR="#990000" WIDTH="1"/>
+<node COLOR="#111111" CREATED="1677179419313" ID="ID_632273995" MODIFIED="1677180436320" TEXT="python">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1677180816119" ID="ID_943730764" MODIFIED="1677181576511" TEXT="EngineerSpock">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1677182723283" ID="ID_155599295" MODIFIED="1677183056612" TEXT="https://www.youtube.com/watch?v=l55idoDKw_g&amp;list=PLBheEHDcG7-mtn0lwo3F2kbojtewF6bL0">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1677180829926" ID="ID_849271230" MODIFIED="1677181576512" TEXT="Cisco - skills for all">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1677182841010" ID="ID_1491850121" MODIFIED="1677183056614" TEXT="https://skillsforall.com/course/python-essentials-1?userLang=en-US">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1677180873344" ID="ID_8017898" MODIFIED="1677181576513" TEXT="leetcode.com">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1677179425267" ID="ID_1465795222" MODIFIED="1677180436321" TEXT="pandas, numpy, matplotlib, pyplot">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1677179466821" ID="ID_548912817" MODIFIED="1677180436322" TEXT="SQL, PostreSQL">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1677182594590" ID="ID_705782028" MODIFIED="1677183056618" TEXT="&#x410;&#x43a;&#x430;&#x434;&#x435;&#x43c;&#x438;&#x44f; &#x425;&#x430;&#x43d;&#x430;">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1677182604067" ID="ID_972665083" MODIFIED="1677183056625" TEXT="https://www.youtube.com/watch?v=v6uqmHNZ2CI&amp;list=PLxGo9dxQkqWCRWVUIos_C_kqiniO8CRt0&amp;index=2">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1677182609526" ID="ID_955276113" MODIFIED="1677183056628" TEXT="EngineerSpock">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1677182616896" ID="ID_1586438693" MODIFIED="1677183056629" TEXT="https://www.youtube.com/watch?v=HVQNxdI6fqY&amp;list=PLBheEHDcG7-k1Y_Uy04Dj2ylWhcfSfqoF">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1677179780931" ID="ID_304553914" MODIFIED="1677180436323" TEXT="git">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1677179784286" ID="ID_1940256623" MODIFIED="1677180436324" TEXT="jupiter / anaconda">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1677179794257" ID="ID_969791520" MODIFIED="1677180436325" TEXT="colab">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1677179796574" ID="ID_758880720" MODIFIED="1677180436325" TEXT="pycharm">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1677179828865" ID="ID_762137201" MODIFIED="1677180436326" TEXT="docker">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1677179709685" ID="ID_1078291037" MODIFIED="1677182896677" TEXT="ml frameworks">
+<edge COLOR="#990000" WIDTH="1"/>
+<node COLOR="#111111" CREATED="1677179551954" ID="ID_5212229" MODIFIED="1677180436328" TEXT="tensorflow">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1677179490516" ID="ID_1629718369" MODIFIED="1677180436329" TEXT="pytorch">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1677179852218" ID="ID_1358858575" MODIFIED="1677182188058" TEXT="&#x434;&#x43e;&#x43f;&#x43e;&#x43b;&#x43d;&#x438;&#x442;&#x435;&#x43b;&#x44c;&#x43d;&#x43e;">
+<edge COLOR="#990000" WIDTH="1"/>
+<node COLOR="#111111" CREATED="1677179861988" ID="ID_1235669248" MODIFIED="1677180436331" TEXT="dwh">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1677179869591" ID="ID_1934192924" MODIFIED="1677180436332" TEXT="data lake">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1677179873165" ID="ID_395294384" MODIFIED="1677180436333" TEXT="clickhouse">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1677179881959" ID="ID_1510167856" MODIFIED="1677180793740">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#1071;&#1085;&#1076;&#1077;&#1082;&#1089;&#1055;&#1088;&#1072;&#1082;&#1090;&#1080;&#1082;&#1091;&#1084;
+    </p>
+    <p>
+      https://practicum.yandex.ru/profile/ycloud-corp/
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1677179916809" ID="ID_1251669539" MODIFIED="1677180436334" TEXT="etl">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1677179349208" ID="ID_210176040" MODIFIED="1677182923728" POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">&#1058;&#1077;&#1086;&#1088;&#1077;&#1090;&#1080;&#1095;&#1077;&#1089;&#1082;&#1080;&#1077; &#1085;&#1072;&#1074;&#1099;&#1082;&#1080;</font>
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#00b439" WIDTH="2"/>
+<node COLOR="#990000" CREATED="1677181549690" ID="ID_1958497868" MODIFIED="1677183011605">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#1050;&#1091;&#1088;&#1089;&#1099; &#1086;&#1090;:
+    </p>
+    <p>
+      &#1056;&#1069;&#1059; &#1080;&#1084;. &#1055;&#1083;&#1077;&#1093;&#1072;&#1085;&#1086;&#1074;&#1072; &quot;&#1054;&#1089;&#1085;&#1086;&#1074;&#1099; DS &#1085;&#1072; Python&quot;
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      stepik.org
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      3Blue1Brown
+    </p>
+    <p>
+      https://www.youtube.com/watch?v=RNTRYicPvWQ&amp;list=PLVjLpKXnAGLXPaS7FRBjd5yZeXwJxZil2&amp;index=3
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#990000" WIDTH="1"/>
+<node COLOR="#111111" CREATED="1677180237817" ID="ID_451614177" MODIFIED="1677181576526" TEXT="&#x43b;&#x438;&#x43d;&#x435;&#x439;&#x43d;&#x430;&#x44f; &#x430;&#x43b;&#x433;&#x435;&#x431;&#x440;&#x430;">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1677180255908" ID="ID_1638074219" MODIFIED="1677181576527" TEXT="&#x43c;&#x430;&#x442;&#x435;&#x43c;&#x430;&#x442;&#x438;&#x447;&#x435;&#x441;&#x43a;&#x438;&#x439; &#x430;&#x43d;&#x430;&#x43b;&#x438;&#x437;">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1677180268984" ID="ID_1354852148" MODIFIED="1677182263587" TEXT="&#x441;&#x442;&#x430;&#x442;&#x438;&#x441;&#x442;&#x438;&#x43a;&#x430;">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1677180250435" ID="ID_1326164912" MODIFIED="1677181576529" TEXT="&#x442;&#x435;&#x43e;&#x440;&#x438;&#x44f; &#x432;&#x435;&#x440;&#x43e;&#x44f;&#x442;&#x43d;&#x43e;&#x441;&#x442;&#x435;&#x439;">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1677182924686" ID="ID_241944940" MODIFIED="1677183056642" TEXT="&#x41a;&#x443;&#x440;&#x441; &#x43f;&#x43e; &#x43c;&#x430;&#x448;&#x438;&#x43d;&#x43d;&#x43e;&#x43c;&#x443; &#x43e;&#x431;&#x443;&#x447;&#x435;&#x43d;&#x438;&#x44e;">
+<edge COLOR="#990000" WIDTH="1"/>
+<node COLOR="#111111" CREATED="1677183034404" ID="ID_1430289530" MODIFIED="1677183056644" TEXT="https://www.youtube.com/watch?v=dEFxoyJhm3Y&amp;list=PLZSPcxXPEZVoaCDSEtrrSq2i_ewYqrzZE">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1677183040836" ID="ID_262219664" MODIFIED="1677183056645" TEXT="ods.ai">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1677181753528" HGAP="21" ID="ID_462667684" MODIFIED="1677182242121" POSITION="right" TEXT="&#x41f;&#x440;&#x430;&#x43a;&#x442;&#x438;&#x447;&#x435;&#x441;&#x43a;&#x438;&#x435; &#x43d;&#x430;&#x432;&#x44b;&#x43a;&#x438; &#x440;&#x435;&#x448;&#x435;&#x43d;&#x438;&#x44f; DS &#x437;&#x430;&#x434;&#x430;&#x447;" VSHIFT="42">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#990000" CREATED="1677181769948" ID="ID_1164022323" MODIFIED="1677182233718" TEXT="Kaggle">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1677182215662" ID="ID_1251223730" MODIFIED="1677182233719" TEXT="ods.ai">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+</node>
+</map>
